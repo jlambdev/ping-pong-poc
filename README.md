7e7f046ce1e556c4e6d3cbf7f50adcbb7713085f
Jonathan Lamb <jlamb.8153@gmail.com>
2019-12-27 13:32:56 +0000

    Add API call to saga code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## TODO
 
--   Use environment variables for UI
+-   Fix CORS issue from :3000 to API on :4000
 -   Get UI to talk to API
 -   Fix UI tests
 -   Fix yarn errors (could be to do with Mac OS Catalina)
--- ui/src/redux/sagas.ts
+++ ui/src/redux/sagas.ts
@@ -1,13 +1,38 @@
-import { takeLatest, delay, put } from 'redux-saga/effects';
+import { takeLatest, delay, put, call, CallEffect } from 'redux-saga/effects';
+
+import config from '../config/environment';
 
 import { PING } from './actionTypes';
 import { pong, reset } from './actions';
 
+const DEFAULT_RESPONSE = { httpStatus: 200, text: 'PONG' };
+
 function* sendPing(): Generator {
-    yield delay(1000);
-    yield put(pong({ httpStatus: 200, text: 'PONG' }));
-    yield delay(2000);
-    yield put(reset());
+    if (config.ENABLE_API && !!config.API_URI) {
+        const response: Response | any = yield call(() =>
+            fetch(config.API_URI),
+        );
+
+        if (!response.httpStatus) {
+            throw Error(
+                'There was a problem fetching a response from the Ping Pong API',
+            );
+        }
+
+        yield put(
+            pong({
+                httpStatus: response.httpStatus,
+                text: response.message,
+            }),
+        );
+        yield delay(2000);
+        yield put(reset());
+    } else {
+        yield delay(1000);
+        yield put(pong(DEFAULT_RESPONSE));
+        yield delay(2000);
+        yield put(reset());
+    }
 }
 
 export default function* rootSaga(): Generator {
